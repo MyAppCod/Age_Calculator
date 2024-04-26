cfa14f4acf579807a160bc7b57f2d7584e84134d
MyAppCod <166295827+MyAppCod@users.noreply.github.com>
2024-04-26 18:56:17 +0530

    updated

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
- function getDOB() {
+const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-let birth = document.getElementById("DOB").value;
+function getDOB() {
 
-let dob = new Date(birth);
-let dayBirth = dob.getDate();
-let monthBirth = dob.getMonth();
-let yearBirth = dob.getFullYear();
+  let birth = document.getElementById("DOB").value;
 
-let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let dob = new Date(birth);
+  let dayBirth = dob.getDate();
+  let monthBirth = dob.getMonth();
+  let yearBirth = dob.getFullYear();
 
-let now = new Date(document.getElementById("CD").value);
 
-let yearNow = now.getFullYear();
-let monthNow = now.getMonth();
-let dayNow = now.getDate();
+  let now = new Date(document.getElementById("CD").value);
+
+  let yearNow = now.getFullYear();
+  let monthNow = now.getMonth();
+  let dayNow = now.getDate();
 
   if (dayBirth > dayNow) {
     dayNow = dayNow + month[monthNow - 1];
@@ -23,15 +24,18 @@ let dayNow = now.getDate();
     monthNow = monthNow + 12;
     yearNow = yearNow - 1;
   }
-  var d = dayNow - dayBirth;
+  var d = dayNow - dayBirth; 
   var m = monthNow - monthBirth;
   var y = yearNow - yearBirth;
 
-if (y > 0){
-        document.getElementById("currentAge").innerHTML = "Your current Age is " + y + " years " + m + " months " + d + " days";
-    if (dayBirth === dayNow && monthBirth === monthNow){
-        document.getElementById("currentAge").innerHTML = "Happy " + y + " years!!! ";
-       }
-    }else
-    document.getElementById("currentAge").innerHTML = "Invalid Entry";
+  if (y > 0) {
+    // const message = `Your current Age is ${y} years ${m} months ${d} days`
+    document.getElementById("currentAge").innerHTML = "Your current Age is " + y + " years " + m + " months " + d + " days";
+
+    if (dayBirth === dayNow && monthBirth === monthNow) {
+      document.getElementById("currentAge").innerHTML = "Happy " + y + " years!!! ";
+    }
+  } else
+    document.getElementById("currentAge").innerText = "Invalid Entry";
+  
 }
